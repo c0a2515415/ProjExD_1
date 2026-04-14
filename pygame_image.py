fd48124e8ce638891b1ca66ddb0417e7abe4ed3f
c0a2515415 <c0a2515415@edu.teu.ac.jp>
2026-04-14 15:36:34 +0900

    練習３と練習４：こうかとん画像の描写

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    k_img = pg.image.load("fig/3.png")
+    k_img = pg.transform.flip(k_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #練習2
+        screen.blit(k_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
