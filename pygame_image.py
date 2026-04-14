d77845972cec7ec88c6b5203cf97447d07c1b5e7
c0a2515415 <c0a2515415@edu.teu.ac.jp>
2026-04-14 16:12:10 +0900

    練習１０：キーを押して移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,8 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     k_img = pg.image.load("fig/3.png")
     k_img = pg.transform.flip(k_img, True, False)
+    k_rct = k_img.get_rect()
+    k_rct.center = 300, 200
     tmr = 0
     x = 0 #練習5
     while True:
@@ -23,7 +25,16 @@ def main():
         screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(k_img, [300, 200])
+        screen.blit(k_img, k_rct)
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            k_rct.move_ip((0, -1))
+        elif key_lst[pg.K_DOWN]:
+            k_rct.move_ip((0, 1))
+        elif key_lst[pg.K_LEFT]:
+            k_rct.move_ip((-1, 0))
+        elif key_lst[pg.K_RIGHT]:
+            k_rct.move_ip((1, 0))
         pg.display.update()
         tmr += 1        
         clock.tick(200) #FPSはこれ
